862ae5e70982af7b3fa00e3aa2e9316fd77c6a4c
Onur Kansoy <onurkansoy01@gmail.com>
2026-01-26 11:35:54 +0300

    feat: implement graph for Birdy and update gitignore

---
+++ src/graph.py
@@ -0,0 +1,43 @@
+from langgraph.graph import StateGraph, START, END
+from src.state import AppState
+
+graph = StateGraph(AppState)
+
+graph.add_node("Decide Any Media", decide_any_media_node)
+
+graph.add_node("Identify Bird with Query", identify_bird_with_query_node)
+graph.add_node("Identify Bird with Photo", identify_bird_with_photo_node)
+graph.add_node("Identify Bird with Sound", identify_bird_with_sound_node)
+
+graph.add_node("Find Bird Info", find_bird_info_node)
+graph.add_node("Find Bird Photos", find_bird_photos_node)
+graph.add_node("Find Bird Sounds", find_bird_sounds_node)
+graph.add_node("Compose", compose_node)
+
+graph.set_entry_point("Decide Any Media")
+graph.add_conditional_edges(
+    "Decide Any Media",
+    route_based_on_media_node,
+    {
+        "photo": "Identify Bird with Photo",
+        "sound": "Identify Bird with Sound",
+        "query": "Identify Bird with Query",
+    },
+)
+graph.add_edge("Identify Bird with Query", "Find Bird Info")
+graph.add_edge("Identify Bird with Query", "Find Bird Photos")
+graph.add_edge("Identify Bird with Query", "Find Bird Sounds")
+
+graph.add_edge("Identify Bird with Photo", "Find Bird Info")
+graph.add_edge("Identify Bird with Photo", "Find Bird Photos")
+graph.add_edge("Identify Bird with Photo", "Find Bird Sounds")
+
+graph.add_edge("Identify Bird with Sound", "Find Bird Info")
+graph.add_edge("Identify Bird with Sound", "Find Bird Photos")
+graph.add_edge("Identify Bird with Sound", "Find Bird Sounds")
+
+graph.add_edge("Find Bird Info", "Compose")
+graph.add_edge("Find Bird Photos", "Compose")
+graph.add_edge("Find Bird Sounds", "Compose")
+
+graph.add_edge("Compose", END)
